c843794974a675efe5484031f662ebf1247611d5
ichaves <ichaves@cys.com.ar>
2018-10-26 16:41:46 -0300

    Integración de estilos - OK

--- TF_Base/App_Start/BundleConfig.cs
+++ TF_Base/App_Start/BundleConfig.cs
@@ -8,23 +8,34 @@ namespace TF_Base
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            BundleTable.EnableOptimizations = false;
+            //STYLES
+            bundles.Add(new StyleBundle("~/bundles/.css")
+                .Include(
+                "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                "~/Content/vendor/font-awesome/css/font-awesome.min.css",
+                "~/Content/vendor/font-awesome/css/sarasa.css",
+                "~/Content/vendor/bootstrap-select/css/bootstrap-select.min.css",
+                "~/Content/vendor/owl.carousel/assets/owl.carousel.css",
+                "~/Content/vendor/owl.carousel/assets/owl.theme.default.css",
+                "~/Content/css/style.default.css",
+                "~/Content/css/custom.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/Site.css"));
+            //SCRIPTS
+            bundles.Add(new ScriptBundle("~/bundles/.js")
+                .Include(
+                "~/Content/vendor/jquery/jquery.min.js",
+                "~/Content/vendor/popper.js/umd/popper.min.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.min.js",
+                "~/Content/vendor/jquery.cookie/jquery.cookie.js",
+                "~/Content/vendor/waypoints/lib/jquery.waypoints.min.js",
+                "~/Content/vendor/jquery.counterup/jquery.counterup.min.js",
+                "~/Content/vendor/owl.carousel/owl.carousel.min.js",
+                "~/Content/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js",
+                "~/Content/js/jquery.parallax-1.1.3.js",
+                "~/Content/vendor/bootstrap-select/js/bootstrap-select.min.js",
+                "~/Content/vendor/jquery.scrollto/jquery.scrollTo.min.js",
+                "~/Content/js/front.js"));
         }
     }
 }
