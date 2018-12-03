ba7ce11d144a931a6dae0655f23ccdfd9aa4c165
ichaves <ichaves@cys.com.ar>
2018-12-02 22:21:46 -0300

    Manejo Stock, alerta poco stock, ABM Sucursal

---
+++ TF_Base/Areas/Admin/Controllers/StocksController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TF_Base.Models;
+
+namespace TF_Base.Areas.Admin.Controllers
+{
+    public class StocksController : Controller
+    {
+        private ecommerceEntities db = new ecommerceEntities();
+
+        // GET: Admin/Stocks
+        public ActionResult Index()
+        {
+            var stock = db.Stock.Include(s => s.Producto).Include(s => s.Sucursal);
+            ViewBag.Sucursales = GetSucursalesList();
+            ViewBag.SeleccionSucursal = "Casa Central";
+            return View(stock.Where(s => s.Sucursal.descripcion == "Casa Central").ToList());
+        }
+
+        private SelectList GetSucursalesList()
+        {
+            SelectList sucursalesList = new SelectList(db.Sucursal.ToList(), "idSucursal", "descripcion");
+            return sucursalesList;
+        }
+
+        private string GetSucursalSelect(string value) 
+        {
+            int idsucursal = int.Parse(value != "" ? value : "0");
+            return idsucursal != 0 ? db.Sucursal.Where(s => s.idSucursal == idsucursal).FirstOrDefault().descripcion : "sin selección";
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            var stock = db.Stock.Include(s => s.Producto).Include(s => s.Sucursal);
+            ViewBag.Sucursales = GetSucursalesList();
+            string sucursalDesc = GetSucursalSelect(form.GetValue("ddlSucursales").AttemptedValue);
+            ViewBag.SeleccionSucursal = sucursalDesc;
+            return View(stock.Where(s => s.Sucursal.descripcion.Contains(sucursalDesc)).ToList());
+        }
+        // GET: Admin/Stocks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = db.Stock.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // GET: Admin/Stocks/Create
+        public ActionResult Create()
+        {
+            ViewBag.idProducto = new SelectList(db.Producto, "idProducto", "descripcion");
+            ViewBag.idSucursal = new SelectList(db.Sucursal, "idSucursal", "descripcion");
+            return View();
+        }
+
+        // POST: Admin/Stocks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idStock,idSucursal,idProducto,cantidad,minimo,maximo")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stock.Add(stock);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idProducto = new SelectList(db.Producto, "idProducto", "descripcion", stock.idProducto);
+            ViewBag.idSucursal = new SelectList(db.Sucursal, "idSucursal", "descripcion", stock.idSucursal);
+            return View(stock);
+        }
+
+        // GET: Admin/Stocks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = db.Stock.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idProducto = new SelectList(db.Producto, "idProducto", "descripcion", stock.idProducto);
+            ViewBag.idSucursal = new SelectList(db.Sucursal, "idSucursal", "descripcion", stock.idSucursal);
+            return View(stock);
+        }
+
+        // POST: Admin/Stocks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idStock,idSucursal,idProducto,cantidad,minimo,maximo")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stock).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idProducto = new SelectList(db.Producto, "idProducto", "descripcion", stock.idProducto);
+            ViewBag.idSucursal = new SelectList(db.Sucursal, "idSucursal", "descripcion", stock.idSucursal);
+            return View(stock);
+        }
+
+        // GET: Admin/Stocks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stock stock = db.Stock.Find(id);
+            if (stock == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stock);
+        }
+
+        // POST: Admin/Stocks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Stock stock = db.Stock.Find(id);
+            db.Stock.Remove(stock);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TF_Base/Areas/Admin/Controllers/SucursalsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TF_Base.Models;
+
+namespace TF_Base.Areas.Admin.Controllers
+{
+    public class SucursalsController : Controller
+    {
+        private ecommerceEntities db = new ecommerceEntities();
+
+        // GET: Admin/Sucursals
+        public ActionResult Index()
+        {
+            var sucursal = db.Sucursal.Include(s => s.Direccion);
+            return View(sucursal.ToList());
+        }
+
+        // GET: Admin/Sucursals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursal sucursal = db.Sucursal.Find(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // GET: Admin/Sucursals/Create
+        public ActionResult Create()
+        {
+            ViewBag.idDireccion = new SelectList(db.Direccion, "idDireccion", "direccion1");
+            return View();
+        }
+
+        // POST: Admin/Sucursals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idSucursal,idDireccion,telefono,email,descripcion")] Sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sucursal.Add(sucursal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idDireccion = new SelectList(db.Direccion, "idDireccion", "direccion1", sucursal.idDireccion);
+            return View(sucursal);
+        }
+
+        // GET: Admin/Sucursals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursal sucursal = db.Sucursal.Find(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idDireccion = new SelectList(db.Direccion, "idDireccion", "direccion1", sucursal.idDireccion);
+            return View(sucursal);
+        }
+
+        // POST: Admin/Sucursals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idSucursal,idDireccion,telefono,email,descripcion")] Sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sucursal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idDireccion = new SelectList(db.Direccion, "idDireccion", "direccion1", sucursal.idDireccion);
+            return View(sucursal);
+        }
+
+        // GET: Admin/Sucursals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursal sucursal = db.Sucursal.Find(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // POST: Admin/Sucursals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sucursal sucursal = db.Sucursal.Find(id);
+            db.Sucursal.Remove(sucursal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
