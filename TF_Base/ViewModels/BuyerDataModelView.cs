f31e58212f145425df64312d325c6c91dca8e71f
el dock <lea_eldoc@hotmail.com>
2018-12-11 12:00:20 -0300

    Orden de compra y algunas mejoras, delta y db

---
+++ TF_Base/ViewModels/BuyerDataModelView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TF_Base.ViewModels
+{
+    public class BuyerDataModelView
+    {
+        public MetodoEntrega MetodoEntrega { get; set; }
+        public bool UsarDireccionDeContacto { get; set; }
+
+        public MetodoPago MetodoDePago { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required]
+        public string Telefono { get; set; }
+
+        [Required]
+        public string Mail { get; set; }
+
+        public DireccionEntregaViewModel Direccion { get; set; }
+    }
+
+    public enum MetodoEntrega
+    {
+        RetiroEnSucursal,
+        EnvioDomicilio
+    }
+
+    public enum MetodoPago
+    {
+        Efectivo,
+        Credito
+    }
+}
